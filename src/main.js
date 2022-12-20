39f60aaeb1ac2fb06e66fe875ff59d9adfd569a1
Noahgamerrr <noah.arsic@gmx.at>
2022-12-20 09:00:58 +0100

    Added the xhr2 package and created an example code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+import XMLHttpRequest from 'xhr2';
+
+function getDef() {
+    const Http = new XMLHttpRequest();
+    const regex = /<meta\sname="description".*>/g;
+    const url='https://www.dictionary.com/browse/corrugated-iron';
+    Http.open("GET", url);
+    
+    Http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            printDef(Http.responseText.match(regex));
+        }
+    }
+
+    Http.send();
+}
+
+getDef();
+
+function printDef(def) {
+    console.log(def);
+}
