88c8e3d531baa7f64dfbc90d833f3db3e39a141a
Noahgamerrr <noah.arsic@gmx.at>
2022-12-20 11:35:48 +0100

    it is possible to look for different word types

--- src/main.js
+++ src/main.js
@@ -4,19 +4,39 @@ const pup = require("puppeteer");
     const browser = await pup.launch();
     const page = await browser.newPage();
 
+    const typeReq = "verb"
+
     await page.goto("https://www.dictionary.com/browse/permit");
 
-    const allResultsSelector = '.css-10n3ydx';
-    await page.waitForSelector(allResultsSelector);
+    const typeSelector = '.css-69s207';
+    await page.waitForSelector(typeSelector);
+
+    const type = await page.evaluate(allResultsSelector => {
+        return [...document.querySelectorAll(allResultsSelector)].map(anchor => {
+          const title = anchor.textContent.split(' ')[0].trim();
+          return `${title} - ${anchor.href}`;
+        });
+      }, typeSelector);
+
+    const defSelector = '.css-10n3ydx';
+    await page.waitForSelector(defSelector);
 
     const def = await page.evaluate(allResultsSelector => {
         return [...document.querySelectorAll(allResultsSelector)].map(anchor => {
-          const title = anchor.textContent.split('|')[0].trim();
+          const title = anchor.textContent.trim();
           return `${title} - ${anchor.href}`;
         });
-      }, allResultsSelector);
+      }, defSelector);
 
-    console.log(def.join('\n'));
+    const res = [];
+
+    for (let i = 0; i < def.length; i++) {
+        if (type[i].split(" ")[0] == typeReq) {
+            for (let data of def[i].split(/[\.:]/g)) res.push(data);
+        }
+    }
+
+    console.log(res[0]);
     
 
     await browser.close();
